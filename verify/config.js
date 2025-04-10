fddcf0527e4b2249ce7ceb00c5a18a6eae540d9d
Vermillion <83882768+vermypoo@users.noreply.github.com>
2025-04-10 19:23:47 +0100

    Delete verify/config.js

--- verify/config.js
+++
@@ -1,8 +0,0 @@
-(function() {
-  const encodedWebhook = "aHR0cHM6Ly9kaXNjb3JkLmNvbS9hcGkvd2ViaG9va3MvMTM1OTgyNzk0MDU5ODIyMjk4OS9YcVFIbHZnTXRhRXV0aHAzX3RONm9OUDFWcU5xZURGZG1UdDF2SlpPUzNGRXBLVTBmdm8zRlBtOUM1XzdwM09fVllFSg==";
-  const encodedVpnKey  = "ZDQxNzRhYzg3OWVjNDVkYmFmNjhmMDYyMmFjNTUzYmE=";
-  window.config = {
-    webhookUrl: atob(encodedWebhook),
-    vpnApiKey: atob(encodedVpnKey)
-  };
-})();
